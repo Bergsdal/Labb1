61d3d3efa739dc19bfaf209d41b6101f5a11ad09
Bergsdal <bergsdal@kth.se>
2016-11-02 15:17:06 +0100

    Initial commit

---
+++ src/Human.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Dexter on 2016-11-01.
+ */
+public class Human {
+    private int age;
+    private String name;
+
+    public Human(int ageIn, String nameIn){
+        age = ageIn;
+        name = nameIn;
+    }
+
+    public String toString(){
+        return "ålder: " + age + ", namn: " + name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Human putte = new Human(25,"Putte");
+        putte.getAge();
+        putte.getName();
+        System.out.println(putte.toString());
+    }
+}
